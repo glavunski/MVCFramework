44b4506a2922f2577ff2136a8187d74e9b991e83
glavunski <xiii1994@abv.bg>
2015-05-10 02:13:02 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,7 +5,7 @@ define('DEFAULT_ACTION', 'index');
 define('DEFAULT_VIEW', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-define('DB_HOST', 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/');
+define('DB_HOST', 'https://my-mvcblog.rhcloud.com/phpmyadmin/');
 define('DB_USER', 'adminGHgLAl8');
 define('DB_PASS', '__iLiFsmX2jy');
 define('DB_NAME', 'my');
