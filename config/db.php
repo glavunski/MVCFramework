78766af11eca8840778af53ad47ba83050c2b75b
glavunski <xiii1994@abv.bg>
2015-05-10 02:08:10 +0300

    Update db.php

--- config/db.php
+++ config/db.php
@@ -5,10 +5,10 @@ define('DEFAULT_ACTION', 'index');
 define('DEFAULT_VIEW', 'index');
 define('DEFAULT_LAYOUT', 'default');
 
-define('DB_HOST', 'localhost');
-define('DB_USER', 'root');
-define('DB_PASS', '123321');
-define('DB_NAME', 'mvc');
+define('DB_HOST', 'mysql://$OPENSHIFT_MYSQL_DB_HOST:$OPENSHIFT_MYSQL_DB_PORT/');
+define('DB_USER', 'adminGHgLAl8');
+define('DB_PASS', '__iLiFsmX2jy');
+define('DB_NAME', 'my');
 
 define('INFO_MESSAGES_SESSION_KEY', 'infoMessages');
 define('ERROR_MESSAGES_SESSION_KEY', 'errorMessages');
